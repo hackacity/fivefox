7dc3d921f09ba33f2eff3973d0eee652b879c514
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-02 16:46:33 -0300

    removido react js

--- web-app/app/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './components/Home'
-
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Server running in Development mod!');
-}
-
-ReactDOM.render(<Home />, document.getElementById('app'))
