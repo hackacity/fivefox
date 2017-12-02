9046403193ea3e6fa63ac6ee5378486bda868ea4
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-02 20:50:04 -0300

    // WIP api não possui permissão de CORS

--- web-app/src/app/api.service.ts
+++ web-app/src/app/api.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class APIService {
 
-  constructor() { }
+    URI_BASE = "http://127.0.0.1:5000/";
+
+    constructor(private http : Http) { }
+
+    getTest() : Observable<any> {
+
+        let uri = this.URI_BASE + 'incidentes';
+
+        return this.http
+                    .get(uri)
+                    .map(res => res.json())
+                    .catch(error => Observable.throw(error.json().error || 'Server error'));
+
+    }
+
 
 }
--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+import { APIService } from './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hack a City';
+export class AppComponent implements AfterViewInit {
+
+    title = 'Hack a City';
+
+    constructor(private service : APIService) {}
+
+    ngAfterViewInit() {
+
+        this.service.getTest().subscribe(text => console.log(text), err => console.error(err));
+
+    }
+
+
 }
