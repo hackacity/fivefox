452c3eb8ec0b88738d6a56ebe495393ff01d6d8b
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-03 00:32:02 -0300

    Adicionado Método que recupera BOs

--- web-app/src/app/mapa/mapa.component.ts
+++ web-app/src/app/mapa/mapa.component.ts
@@ -1,5 +1,6 @@
 import { BrowserService } from './../browser.service';
 import { Component, OnInit } from '@angular/core';
+import { APIService } from 'app/api.service';
 
 declare var google;
 declare var MarkerClusterer;
@@ -13,33 +14,10 @@ export class MapaComponent implements OnInit {
 
     map: any = {};
 
-    locations = [
-        {lat: -31.563910, lng: 147.154312},
-        {lat: -33.718234, lng: 150.363181},
-        {lat: -33.727111, lng: 150.371124},
-        {lat: -33.848588, lng: 151.209834},
-        {lat: -33.851702, lng: 151.216968},
-        {lat: -34.671264, lng: 150.863657},
-        {lat: -35.304724, lng: 148.662905},
-        {lat: -36.817685, lng: 175.699196},
-        {lat: -36.828611, lng: 175.790222},
-        {lat: -37.750000, lng: 145.116667},
-        {lat: -37.759859, lng: 145.128708},
-        {lat: -37.765015, lng: 145.133858},
-        {lat: -37.770104, lng: 145.143299},
-        {lat: -37.773700, lng: 145.145187},
-        {lat: -37.774785, lng: 145.137978},
-        {lat: -37.819616, lng: 144.968119},
-        {lat: -38.330766, lng: 144.695692},
-        {lat: -39.927193, lng: 175.053218},
-        {lat: -41.330162, lng: 174.865694},
-        {lat: -42.734358, lng: 147.439506},
-        {lat: -42.734358, lng: 147.501315},
-        {lat: -42.735258, lng: 147.438000},
-        {lat: -43.999792, lng: 170.463352}
-      ];
-
-    constructor(private browserService: BrowserService) { }
+    constructor(
+        private browserService: BrowserService,
+        private api: APIService
+    ) { }
 
     ngOnInit() {
 
@@ -47,6 +25,13 @@ export class MapaComponent implements OnInit {
             zoom: 3,
             center: { lat: -28.024, lng: 140.887 }
         });
+
+        this.setLocationOnMap();
+        this.setIncidentesOnMap();
+
+    }
+
+    setLocationOnMap() {
 
         this.browserService.getLocation().subscribe(latlng => {
 
@@ -58,18 +43,27 @@ export class MapaComponent implements OnInit {
             this.map.setCenter(pos);
         });
 
+    }
+
+    setIncidentesOnMap() {
+        let locations = this.api.getIncidentes();
+
+        console.log(locations);
+
+
         var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-        var markers = this.locations.map(function (location, i) {
+
+        var markers = locations.map(function (location, i) {
             return new google.maps.Marker({
                 position: location,
                 label: labels[i % labels.length]
             });
         });
 
-        // Add a marker clusterer to manage the markers.
         var markerCluster = new MarkerClusterer(this.map, markers,
             { imagePath: 'http://localhost:4200/assets/images/m' });
+
 
     }
 
