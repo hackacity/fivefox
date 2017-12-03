8a7df899687d74d65fa576df2a7054a8a665c721
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-03 00:11:39 -0300

    Cluster de Imagens :)

--- web-app/src/app/mapa/mapa.component.ts
+++ web-app/src/app/mapa/mapa.component.ts
@@ -2,6 +2,7 @@ import { BrowserService } from './../browser.service';
 import { Component, OnInit } from '@angular/core';
 
 declare var google;
+declare var MarkerClusterer;
 
 @Component({
     selector: 'app-mapa',
@@ -12,13 +13,39 @@ export class MapaComponent implements OnInit {
 
     map: any = {};
 
+    locations = [
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124},
+        {lat: -33.848588, lng: 151.209834},
+        {lat: -33.851702, lng: 151.216968},
+        {lat: -34.671264, lng: 150.863657},
+        {lat: -35.304724, lng: 148.662905},
+        {lat: -36.817685, lng: 175.699196},
+        {lat: -36.828611, lng: 175.790222},
+        {lat: -37.750000, lng: 145.116667},
+        {lat: -37.759859, lng: 145.128708},
+        {lat: -37.765015, lng: 145.133858},
+        {lat: -37.770104, lng: 145.143299},
+        {lat: -37.773700, lng: 145.145187},
+        {lat: -37.774785, lng: 145.137978},
+        {lat: -37.819616, lng: 144.968119},
+        {lat: -38.330766, lng: 144.695692},
+        {lat: -39.927193, lng: 175.053218},
+        {lat: -41.330162, lng: 174.865694},
+        {lat: -42.734358, lng: 147.439506},
+        {lat: -42.734358, lng: 147.501315},
+        {lat: -42.735258, lng: 147.438000},
+        {lat: -43.999792, lng: 170.463352}
+      ];
+
     constructor(private browserService: BrowserService) { }
 
     ngOnInit() {
 
         this.map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: {lat: -16, lng: -68}
+            zoom: 3,
+            center: { lat: -28.024, lng: 140.887 }
         });
 
         this.browserService.getLocation().subscribe(latlng => {
@@ -29,7 +56,20 @@ export class MapaComponent implements OnInit {
             };
 
             this.map.setCenter(pos);
-        });        
+        });
+
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        var markers = this.locations.map(function (location, i) {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(this.map, markers,
+            { imagePath: 'http://localhost:4200/assets/images/m' });
 
     }
 
