2274359de87f0638041cf3e1d09c9d6853a022c7
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-12-03 02:34:43 -0300

    Api Hacker da Shih

--- backend/api.py
+++ backend/api.py
@@ -3,7 +3,8 @@ from flask_restful import reqparse, abort, Api, Resource
 
 import nltk
 from flask_cors import CORS
-
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': '172.50.5.140', 'port': 9200}])
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,6 +29,35 @@ IncidentesDados = {
             'incidente': 'briga de rua',
 
         }
+}
+
+IncidentesOcorridos = { 
+    'ocorrencias' : [
+        {
+            'bairro': 'vila coophamorena',
+            'ano': 2011, 
+            'latitude': -20.4665648,
+            'dia': '15',
+            'tipo': 'trafico de drogas', 
+            'longitude': -54.6031665,
+            'objeto': 'celular',
+            'hora': 17,
+            'diaDaSemana': 'sexta-feira',
+            'mes': 7
+        }, 
+        {
+            'bairro': 'vila olga',
+            'ano': 2011,
+            'latitude': -20.4650274,
+            'dia': '15',
+            'tipo': 'furto', 
+            'longitude': -54.6262041,
+            'objeto': 'produtos',
+            'hora': 6, 
+            'diaDaSemana': 'quarta-feira', 
+            'mes': 6
+        }
+        ] 
 }
 
 def abort_if_todo_doesnt_exist(todo_id):
@@ -70,13 +100,76 @@ class TodoList(Resource):
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
 
+class InfoTotal(Resource):
+    def get(self):
+        dado = {}
+        dado['total'] = es.search(index='bos', body={
+				        'size' : 0,
+				        'query': {
+				            'match_all' : {}
+				       }
+				   }
+				)['hits']['total']
+
+        dado['incidentes'] = []
+
+        dado['incidentes'].append({'roubo':  es.search(index='bos', body={"filter": 
+                {"match": 
+                    {"fact": "roubo"}
+                }
+            }, size=0
+        )['hits']['total']})
+
+        dado['incidentes'].append({'furto': es.search(index='bos', body={"filter": 
+                {"match": 
+                    {"fact": "furto"}
+                }
+            }, size=0
+        )['hits']['total']})
+
+        dado['incidentes'].append({'homicidio': es.search(index='bos', body={"filter": 
+                {"match": 
+                    {"fact": "homicidio"}
+                }
+            }, size=0
+        )['hits']['total']})
+
+        dado['incidentes'].append({'tentativa de homicidio': es.search(index='bos', body={"filter": 
+                {"match": 
+                    {"fact":{
+
+                        "query":"tentativa de homicidio",
+                    "operator" : "and"	
+                    }
+                    }
+                    
+                }
+            }, size=0
+        )['hits']['total']})
+
+        dado['incidentes'].append({"trafico de drogas": es.search(index='bos', body={"filter": 
+                {"match": 
+                    {"fact":{
+
+                        "query":"trafico de drogas",
+                    "operator" : "and"	
+                    }
+                    }
+                    
+                }
+            }, size=0
+        )['hits']['total']})
+
+        return dado
+
 class Incidentes(Resource):
     def get(self):
-        return IncidentesDados
+        return IncidentesOcorridos
 
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<todo_id>')
 api.add_resource(Incidentes, '/incidentes')
+api.add_resource(InfoTotal, '/info/total')
 
 
 if __name__ == '__main__':
